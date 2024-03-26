7613e9901209c089aa8778d363757bb50ada69b6
ongsici <ong_si_ci@htx.gov.sg>
2024-03-26 13:46:02 +0800

    feat: wrapped around FastAPI with 2 endpoints - embed and query

---
+++ api/api_data_models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import List
+
+class FilePath(BaseModel):
+    file_path: str
+
+class QueryInput(BaseModel):
+    model_option: str
+    vectorstore_id: str
+    user_query: str
+
+class SummaryInput(BaseModel):
+    model_option: str
+    pages: List
