e0af2b01398aeb2f49351758b885450190be5147
ongsici <ong_si_ci@htx.gov.sg>
2024-04-19 11:44:31 +0800

    chore: updated margin for chatbot message, arrow for second message and implemented newline rendering

--- frontend/src/components/Bot/config.js
+++ frontend/src/components/Bot/config.js
@@ -6,6 +6,16 @@ const botName = 'GuruDocsBot';
 const config = {
   initialMessages: [createChatBotMessage(`Hello! What questions do you have about your document?`)],
   botName: botName,
+  customComponents: {
+    botChatMessage: ({ message, loader }) => (
+      <div className="react-chatbot-kit-chat-bot-message">
+        {message.split("\n").map((msg) => (
+          <p style={{ marginBlock: "0px" }}>{msg}</p>
+        ))}
+        <div className="react-chatbot-kit-chat-bot-message-arrow"></div>
+      </div>
+    ),
+  },
   // customStyles: {
   //   botMessageBox: {
   //     backgroundColor: '#5BBCFF',
