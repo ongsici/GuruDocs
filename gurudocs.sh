41b2aefca73a0b2f2f0a91c74d940d58749d17e3
ongsici <ong_si_ci@htx.gov.sg>
2024-04-28 16:26:37 +0800

    build: updated build scripts

--- gurudocs.sh
+++ gurudocs.sh
@@ -1,6 +1,6 @@
 case $1 in
   start)
-    docker compose -f ./docker-compose.yml --env-file=frontend/.env up -d
+    docker compose -f ./docker-compose.yml --env-file=frontend/.env up 
     ;;
   stop)
     docker compose -f ./docker-compose.yml --env-file=frontend/.env down
